ec46ea7bfbee2caa087dba5e608670ba85a1905e
Aysha munawwara   Rafik <amunawwara@gmail.com>
2025-05-11 16:45:23 +0530

    Delete audio_visual_meg_llm directory

--- audio_visual_meg_llm/simulate_meg.py
+++
@@ -1,7 +0,0 @@
-
-def simulate_meg_description():
-    return '''
-Visual input detected in occipital cortex (sharp spike ~100ms).
-Audio input detected in auditory cortex (broadband activity ~300ms).
-Combined AV response suggests object recognition and semantic retrieval.
-'''
